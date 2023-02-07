5e34f2408da8425b7e052b0eeb595d3551018f30
shermuhammad temirov <temirov_s@qwasar.io>
2023-02-07 12:51:32 +0000

    I jon some functions mylib modul

--- mylib.py
+++ mylib.py
@@ -20,6 +20,24 @@ def drop_teg(teg, data):
 # print(drop_teg("ref", data))
 
 
+def drop_tegs(tegs, data):
+    """
+    params: teg = str, data = str
+    enter <, >, / without symbols in the tag parameter
+    """
+    for teg in tegs:
+        match = f"<{teg}.*</{teg}>"
+        indexs = re.search(match, data)
+        try:
+            indexs = list(indexs.span())
+            data = data.replace(data[indexs[0] : indexs[1]], "")
+        except:
+            continue
+    return data
+
+
+
+
 def drop_ab(abdict, data):
     """
     params: abdict = list[dict], data = str ;
@@ -42,3 +60,14 @@ def drop_ab(abdict, data):
 # txt = "|||||AAA____________________BBB|||||||||VVV_____________________VVV|||||"
 # params = {"AAA" : "BBB", "VVV" : "VVV"}
 # print(drop_ab(params, txt))
+
+
+
+def replaces(replaces, put, data):
+    """
+    params: replaces = list, put = str, data = str;
+    This functio can replace many elements;
+    """
+    for rep in replaces:
+        data = data.replace(rep, put)
+    return data
